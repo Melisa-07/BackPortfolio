0306db83401fa4997a30394c1e869e4017cf8fc1
Melisa Alfonso <alfonsomelisa@hotmail.com>
2024-02-28 01:39:36 -0300

    CrudMasLogin(JWT)

--- src/main/java/com/apbackend/apbackend/ApbackendApplication.java
+++ src/main/java/com/apbackend/apbackend/ApbackendApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 
@@ -15,6 +17,17 @@ public class ApbackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApbackendApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("https://front-portfolio-melisa-s-projects.vercel.app/");
+			}
+		};
+	}
+
 	@Bean
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
